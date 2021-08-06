c6db353f8b24a758562b7d7965f0f622d0a27bac
Marius Sarca <marius.sarca@gmail.com>
2021-08-06 22:03:23 +0300

    Added support for references

--- dist/assembler.js
+++ dist/assembler.js
@@ -1126,6 +1126,7 @@
      * limitations under the License.
      */
     const VAR_REGEX = /@([a-zA-Z0-9\-_]+)/g;
+    const REF_REGEX = /&([a-zA-Z0-9\-_]+)/g;
     const REPLACE_REGEX = /\$(selector|body|class|value|property|state|variants|var)/g;
     class StyleHandler {
         constructor(settings, style, tracker) {
@@ -1214,10 +1215,14 @@
                 value = VALUE_WRAPPER[original](value, original, media, state);
             }
             if (!Array.isArray(value)) {
-                value = Array(properties.length).fill(value.replace(VAR_REGEX, "var(--$1)"));
+                value = Array(properties.length).fill(value
+                    .replace(REF_REGEX, (match, property) => Root.getPropertyValue('--' + property))
+                    .replace(VAR_REGEX, "var(--$1)"));
             }
             else {
-                value = value.map(value => value.replace(VAR_REGEX, "var(--$1)"));
+                value = value.map(value => value
+                    .replace(REF_REGEX, (match, property) => Root.getPropertyValue('--' + property))
+                    .replace(VAR_REGEX, "var(--$1)"));
             }
             const result = [];
             const base = STATE_LIST.length;
